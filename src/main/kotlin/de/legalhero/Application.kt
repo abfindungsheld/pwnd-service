0acc64a2d80620617d79c22366e8b5a25116e125
Philipp Eschenbach <philipp@eschenbach.co>
2023-03-08 09:57:41 +0000

    Initial commit

---
+++ src/main/kotlin/de/legalhero/Application.kt
@@ -0,0 +1,7 @@
+package de.legalhero
+
+import io.micronaut.runtime.Micronaut.run
+fun main(args: Array<String>) {
+	run(*args)
+}
+
---
+++ src/test/groovy/de/legalhero/PwndServiceSpec.groovy
@@ -0,0 +1,19 @@
+package de.legalhero
+
+import io.micronaut.runtime.EmbeddedApplication
+import io.micronaut.test.extensions.spock.annotation.MicronautTest
+import spock.lang.Specification
+import jakarta.inject.Inject
+
+@MicronautTest
+class PwndServiceSpec extends Specification {
+
+    @Inject
+    EmbeddedApplication<?> application
+
+    void 'test it works'() {
+        expect:
+        application.running
+    }
+
+}
